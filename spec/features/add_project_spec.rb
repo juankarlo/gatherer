e6541f9aafd05168da7bc022db96a8bd86a3e1ae
Juan C. Nieves <juanc.nieves@gmail.com>
2015-03-07 20:15:54 -0400

    Fix typo on line 9 add_project_spec.rb

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -6,7 +6,7 @@ describe 'Adding projects' do
     fill_in 'Name', with: 'Project Runway'
     fill_in 'Tasks', with: 'Task 1:3\nTask 2:5'
     click_button 'Create Project'
-    visit project_path
+    visit projects_path
     expect(page).to have_content('Project Runway')
     expect(page).to have_content('8')
   end
